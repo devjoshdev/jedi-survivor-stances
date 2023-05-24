d1131d6a26559059b76f2c9e11c99d48a6382c4d
devjoshdev <jsinanan1@gmail.com>
2023-05-24 00:44:20 -0400

    Define paths for the stances and pass that info to the page

--- pages/stances/[stance-name].js
+++ pages/stances/[stance-name].js
@@ -1,4 +1,19 @@
+import { stancesData } from "@/utils/data";
 export default function Stance(props) {
-
+    return (
+        <>
+            <p>So you are looking for <em>{props["stance-name"]}</em></p>
+        </>
+    );
 };
 
+export async function getStaticPaths() {
+    const paths = stancesData.map((stance) => ({
+        params: { "stance-name": stance.id },
+      })); 
+    return {paths, fallback: false}
+}
+
+export async function getStaticProps(context) {
+    return {props: {"stance-name": context.params["stance-name"]}}
+}
