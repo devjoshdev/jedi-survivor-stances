78f49f35c382308b6596eee41eb3e73847498e67
devjoshdev <jsinanan1@gmail.com>
2023-05-24 00:41:44 -0400

    Add object that stores data for display

---
+++ utils/data.js
@@ -0,0 +1,50 @@
+var stancesData = [];
+
+const singleSaberStance = {
+
+    "id": "single-saber",
+    "description": "This is some test data for the single saber stance",
+    "bullets": ["bullet1", "bullet2"],
+    "imageName": "null"
+
+}
+
+const doubleBladedStance = {
+
+    "id": "double-bladed",
+    "description": "This is some test data for the double bladed stance",
+    "bullets": ["bullet3", "bullet4"],
+    "imageName": "null"
+
+}
+
+const dualWieldStance = {
+
+    "id": "dual-wield",
+    "description": "This is some test data for the dual wield stance",
+    "bullets": ["bullet5", "bullet6"],
+    "imageName": "null"
+
+}
+
+const crossGuardStance = {
+
+    "id": "crossguard",
+    "description": "This is some test data for the crossguard stance",
+    "bullets": ["bullet7", "bullet8"],
+    "imageName": "null"
+
+}
+
+const blasterStance = {
+
+    "id": "blaster",
+    "description": "This is some test data for the blaster stance",
+    "bullets": ["bullet9", "bullet10"],
+    "imageName": "null"
+
+}
+
+stancesData.push(singleSaberStance, doubleBladedStance, dualWieldStance, crossGuardStance, blasterStance);
+
+export {stancesData};
