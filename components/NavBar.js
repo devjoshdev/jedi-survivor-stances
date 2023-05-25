1209e1900bcc95c5a5ed2168ab7e8da1cbee3df8
devjoshdev <jsinanan1@gmail.com>
2023-05-24 22:23:10 -0400

    Change <a> tag to Next.js <Link>

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,11 @@
+import Link from "next/link";
 const NavBar = (props) => {
     const data = props.navElements;
     return (
         <>
             <nav className={"stances-navbar"}>
                 <ul>
-                    {data.map(elem => <a key={elem} href={`/stances/${elem}`}>{elem}</a>)}
+                    {data.map(elem => <Link key={elem} href={`/stances/${elem}`}>{elem}</Link>)}
                 </ul>
             </nav>
         </>
