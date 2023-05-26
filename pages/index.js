62646c32391b7973021d804b15ea2191bbc76b07
devjoshdev <jsinanan1@gmail.com>
2023-05-26 01:21:38 -0400

    upkeep

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import NavBar from "../components/NavBar";
-
+import Head from "next/head";
+const homeStyle = {
+  background: 'radial-gradient(circle, #4E277D, #230E2F)',
+};
 export default function Home() {
   const navElements = ["single-saber", "double-bladed", "dual-wield", "crossguard", "blaster"]
   return (
-    <div>
+    <div style={homeStyle}>
+      <Head>
+      <style>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+            background: ${homeStyle.background};
+          }
+        `}
+        </style>
+      </Head>
       <NavBar navElements={navElements}/>
-      <h1>Hi</h1>
     </div>
   )
 }
