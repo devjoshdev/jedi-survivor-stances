82db2b7499f6dc65328863a57dffe5bb6de5cae1
devjoshdev <jsinanan1@gmail.com>
2023-05-26 01:19:01 -0400

    Add a more star wars like font

--- utils/loadFonts.js
+++ utils/loadFonts.js
@@ -1,5 +1,6 @@
 import { Fira_Code } from "next/font/google";
 import { Poller_One } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 
 const fira = Fira_Code({
     subsets: ['latin'],
@@ -13,5 +14,11 @@ const poller = Poller_One({
     weight: ['400']
 });
 
+const robotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400'],
+});
 
-export {fira, poller};
+
+export {fira, poller, robotoCondensed};
