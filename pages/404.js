9ef40d366f46aa6ba7dc43122e508053f4867ed6
devjoshdev <jsinanan1@gmail.com>
2023-05-28 20:04:20 -0400

    Finish up 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,23 @@
-import {fira} from "../utils/loadFonts";
+import NavBar from "@/components/NavBar";
+import { fira } from "../utils/loadFonts";
 
 const styles = {
-    fontFamily : fira.style.fontFamily
+    fontFamily : fira.style.fontFamily,
+    textAlign: 'center',
+    verticalPadding: '10px',
+    color: 'white',
 }
+const divStyle = {
+    marginTop: '10%',
+};
+const navElements = ["single-saber", "double-bladed", "dual-wield", "crossguard", "blaster"];
 export default function Custom404() {
     return (
-        <div>
+        <div style={divStyle}>
             <h1 style={styles}>404 Error</h1>
             <h2 style={styles}>Page Not Found </h2>
+            <NavBar navElements={navElements}/>
         </div>
     )
-
-// TODO: add NavBar here
 
 };
