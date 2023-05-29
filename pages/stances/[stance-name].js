4634022667507f21b5753353360be372254f6847
devjoshdev <jsinanan1@gmail.com>
2023-05-29 12:12:02 -0400

    Grab appropriate text for the stance and some formatting

--- pages/stances/[stance-name].js
+++ pages/stances/[stance-name].js
@@ -1,11 +1,41 @@
 import { stancesData } from "@/utils/data";
 import NavBar from "@/components/NavBar";
+import Header from "@/components/Header";
+import { robotoCondensed } from "@/utils/loadFonts";
+const textStyle = {
+    textAlign: 'center',
+    textSize: '18px',
+    color: 'white',
+    textShadow: '2px 0px 4px rgba(255, 255, 255, 0.3)',
+    fontFamily: robotoCondensed.style.fontFamily,
+}
+const paragraphStyle = {
+    width: '50%',
+    margin: '0 auto',
+    paddingLeft: '25%',
+    paddingRight: '25%',
+    color: 'white',
+    textShadow: '2px 0px 4px rgba(255, 255, 255, 0.3)',
+    fontFamily: robotoCondensed.style.fontFamily,
+}
+const formatNavElement = (elem) => {
+    let res = [];
+    let words = elem.split('-');
+    words.forEach(word => {
+        res.push(word[0].toUpperCase() + word.substring(1));
+    })
+    let formatted = res.join(' ');
+    return formatted;
+}
 export default function Stance(props) {
+    const focusedStance = stancesData.find(stance => stance.id === props['stance-name']);
     return (
-        <>
+        <div>
+            <Header title={props["stance-name"]}/>
             <NavBar navElements={stancesData.map(stance => stance.id)}/>
-            <p>So you are looking for <em>{props["stance-name"]}</em></p>
-        </>
+            <h1 style={textStyle}>The {formatNavElement(props["stance-name"])}</h1>
+            <p style={paragraphStyle}>{focusedStance.description}</p>
+        </div>
     );
 };
 
